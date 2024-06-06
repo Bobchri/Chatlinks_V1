8a3837537646b5a135288eeafc9302aaa2b89b58
Bobchrix2005 <bobchrix0@gmail.com>
2024-06-06 19:51:13 +0900

    add

---
+++ client/src/pages/register/Register.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import "./register.scss";
+
+const Register = () => {
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h2>Welcome to Chatlinks</h2>
+          <p>
+            Empowering Global Connections: A one stop social , collaboration and business platform. Join the chatlink community to enjoy more
+          </p>
+          <span>Do you have an account?</span>
+          <Link to="/login">
+          <button>Login</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
